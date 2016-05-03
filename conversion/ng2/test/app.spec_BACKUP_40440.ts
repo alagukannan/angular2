f8c8bccde8307781fa615f561f088352737cf760
Alagukannan Alagappan <alagukannan+git@gmail.com>
2016-05-02 19:06:49 -0700

    Angular sample

---
+++ conversion/ng2/test/app.spec_BACKUP_40440.ts
@@ -0,0 +1,13 @@
+/// <reference path="../app/typings/app.d.ts" />
+
+import {SpotifyService} from "../app/ts/services/SpotifyService";
+
+describe('App', () => {
+
+  it('does something', () => {
+  //   let c = new Calculator();
+  //   expect(true).toBe(true);
+  //   expect(c.add(1,2)).toBe(3);
+  });
+
+});
