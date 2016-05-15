64e80b4e8b2677ab8f1670e7aa0b852218e88198
Alagukannan Alagappan <alagukannan+git@gmail.com>
2016-05-14 18:26:19 -0700

    typescript changes

---
+++ typescript/startup.js
@@ -0,0 +1,11 @@
+var startup = (function () {
+    function startup() {
+    }
+    startup.main = function () {
+        console.log("Hello World");
+        return 0;
+    };
+    return startup;
+}());
+startup.main();
+//# sourceMappingURL=startup.js.map
---
+++ typescript/startup.ts
@@ -0,0 +1,8 @@
+class startup{
+    public static main(): number{
+        console.log("Hello World");
+        return 0;
+    }
+}
+
+startup.main();
